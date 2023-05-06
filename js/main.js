f8af81e172aaf5ab7f0c263869f20eb2c96f42bc
pablo08200 <112141544+pablo08200@users.noreply.github.com>
2023-05-05 22:26:44 -0600

    primer repositorio

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*alert();*/ //madar alerts
+ //parte del menu
+ let ubicacionPrincipal = window.pageYOffset; //dara la ubicacion de cada seccion dividida de la pagina
+
+
+  AOS.init();  /*INICIALIZAMOS LA ANIMATION DE AOS ANIMATION PARA LA ILUSTRACION DE ABAJO*/ 
+
+
+ //ayudara a que la barra header desaparezca cuando hagamos scroll abajo y aparezca con scroll arriba 
+ window.addEventListener("scroll",function(){
+    //console.log(this.window.pageYOffset) //dime en que parte de la pagina esta el usuario y print it
+    let dezplazamientoActual = window.pageYOffset; //mide 100   | 200
+    if(ubicacionPrincipal>=dezplazamientoActual){ //0>100? 100>200
+        document.getElementsByTagName("nav")[0].style.top="0px" //toma la info de acuerdo al nombre de etiqueta
+    }else{
+        document.getElementsByTagName("nav")[0].style.top="-100px"  //si baja
+    }
+    ubicacionPrincipal=dezplazamientoActual; //100 200  
+ })
+
+//menu en movimiento
+let enlacesHeader = document.querySelectorAll(".enlades-header")[0];
+let semaforo=true;
+document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
+    if(semaforo){
+        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
+        semaforo=false;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color="#000";
+        semaforo=true;
+    }
+    enlacesHeader.classList.toggle("menudos")
+})
